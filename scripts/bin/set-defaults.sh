b25bd33d39ffafcb39e30a85d4efc670aa2c4f26
Oliver Grawert <ogra@ubuntu.com>
2025-04-09 15:02:39 +0200

    improve set-defaults script

--- scripts/bin/set-defaults.sh
+++ scripts/bin/set-defaults.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
 if ! $(snapctl get default-config-applied); then
-  {
     # connection example
     # rest-helper connect <mysnap> home snapd home
 
     # config example
     # rest-helper set <mysnap> foo bar
-  }
-snapctl set defaults-config-applied=True
+
+    # prevent ourselves from running twice
+    snapctl set defaults-config-applied=True
+fi
